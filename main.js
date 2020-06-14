611fcdfd25d58ce443649aa54911bd93901e2ed9
edwardwang0302 <edwardwang0302@me.com>
2020-06-14 23:49:26 +0800

    fixed setting resources path

--- main.js
+++ main.js
@@ -1,11 +1,37 @@
-const { app, ipcMain, Menu } = require('electron')
+const { app, ipcMain, Menu, dialog } = require('electron')
 const isDev = require('electron-is-dev')
+const { autoUpdater } = require('electron-updater')
 const path = require('path')
 const menuTemplate = require('./src/menuTemplate')
 const AppWindow = require('./src/AppWindow')
 let mainWindow, settingsWindow
 
 app.on('ready', () => {
+  if (isDev) {
+    autoUpdater.updateConfigPath = path.join(__dirname, 'dev-app-update.yml')
+  }
+  autoUpdater.autoDownload = false
+  autoUpdater.checkForUpdates()
+  autoUpdater.on('error', (error) => {
+    dialog.showErrorBox('Error ', error == null? 'unknown': (error.stacktrace))
+  })
+  autoUpdater.on('update-available', () => {
+    dialog.showMessageBox({
+      type: 'info',
+      title: '应用有新版本',
+      message: '发现新版本，是否现在更新?',
+      buttons: ['是', '否']
+    }, (buttonIndex) => {
+      if (buttonIndex === 0)
+        autoUpdater.downloadUpdate()
+    })
+  })
+  autoUpdater.on('update-not-available', () => {
+    // dialog.showMessageBox({
+    //   title: '没有新版本',
+    //   message: '当前已经是最新版本' 
+    // })
+  })
   const mainWindowConfig = {
     width: 1024,
     height: 680,
@@ -22,7 +48,7 @@ app.on('ready', () => {
       height: 400,
       parent: mainWindow
     }
-    const settingsFileLocaton = `file://${path.join(__dirname, './settings/settings.html')}`
+    const settingsFileLocaton = `file://${path.join(__dirname, '../settings/settings.html')}`
     settingsWindow = new AppWindow(settingsWindowConfig, settingsFileLocaton)
     settingsWindow.on('closed', () => {
       mainWindow = null
